af304c0b63045a5d1d24f0a5d98ab1155d64aa4e
Govert de Gans <grappigegovert@hotmail.com>
2020-11-12 16:54:51 +0100

    Patcher: move patching to separate class

--- Patcher/Form1.cs
+++ Patcher/Form1.cs
@@ -15,23 +15,6 @@ namespace Hitman2Patcher
 {
 	public partial class Form1 : Form
 	{
-		[DllImport("kernel32", SetLastError = true)]
-		private static extern IntPtr OpenProcess(uint dwDesiredAccess, bool bInheritHandle, int dwProcessId);
-		[DllImport("kernel32", SetLastError = true)]
-		private static extern bool CloseHandle(IntPtr hObject);
-		[DllImport("kernel32")]
-		private static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr address, byte[] buffer, uint size, out int byteswritten);
-		[DllImport("kernel32.dll", SetLastError = true)]
-		static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr address, [Out] byte[] buffer, uint size, out int numberOfBytesRead);
-		[DllImport("kernel32.dll")]
-		static extern bool VirtualProtectEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, uint flNewProtect, out uint lpflOldProtect);
-
-		private const uint PROCESS_VM_READ = 0x0010; //Required to read memory in a process using ReadProcessMemory.
-		private const uint PROCESS_VM_WRITE = 0x0020; // Required to write to memory in a process using WriteProcessMemory.
-		private const uint PROCESS_VM_OPERATION = 0x0008; // Required to perform an operation on the address space of a process using VirtualProtectEx
-		private const uint PAGE_EXECUTE_READWRITE = 0x40;
-		private const uint PAGE_READWRITE = 0x04;
-
 		Timer timer;
 		HashSet<int> patchedprocesses = new HashSet<int>();
 
@@ -71,22 +54,35 @@ namespace Hitman2Patcher
 		void timer_Tick(object sender, EventArgs e)
 		{
 			Process[] hitmans = Process.GetProcessesByName("HITMAN2");
-			foreach (Process hitman in hitmans)
+			foreach (Process process in hitmans)
 			{
-				if (!patchedprocesses.Contains(hitman.Id))
+				if (!patchedprocesses.Contains(process.Id))
 					try
 					{
-						patch(hitman, Hitman2Version.getVersion(hitman));
+						if (MemoryPatcher.Patch(process, new MemoryPatcher.Options
+						{
+							DisableCertPinning = false,
+							AlwaysSendAuthHeader = true,
+							SetCustomConfigDomain = true,
+							CustomConfigDomain = getSelectedServerHostname(),
+							UseHttp = true
+						}))
+						{
+							log(String.Format("Sucessfully patched processid {0}", process.Id));
+							log(String.Format("Injected server: {0}", getSelectedServerHostname()));
+							patchedprocesses.Add(process.Id);
+						}
+						// else: process not yet ready for patching, try again next timer tick
 					}
 					catch (Win32Exception)
 					{
-						log(String.Format("Failed to patch processid {0}: error code {1}", hitman.Id, Marshal.GetLastWin32Error()));
-						patchedprocesses.Add(hitman.Id);
+						log(String.Format("Failed to patch processid {0}: error code {1}", process.Id, Marshal.GetLastWin32Error()));
+						patchedprocesses.Add(process.Id);
 					}
 					catch (NotImplementedException)
 					{
-						log(String.Format("Failed to patch processid {0}: unknown version", hitman.Id));
-						patchedprocesses.Add(hitman.Id);
+						log(String.Format("Failed to patch processid {0}: unknown version", process.Id));
+						patchedprocesses.Add(process.Id);
 					}
 			}
 		}
@@ -94,57 +90,6 @@ namespace Hitman2Patcher
 		private void log(String msg)
 		{
 			listView1.Items.Insert(0, String.Format("[{0:HH:mm:ss}] - {1}", DateTime.Now, msg));
-		}
-
-		private void patch(Process process, Hitman2Version v)
-		{
-			IntPtr hProcess = OpenProcess(PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_VM_OPERATION, false, process.Id);
-			IntPtr b = process.MainModule.BaseAddress;
-			int byteswritten;
-			uint oldprotectflags;
-			byte[] newurl = Encoding.ASCII.GetBytes(getSelectedServerHostname()).Concat(new byte[] { 0x00 }).ToArray();
-			byte[] http = Encoding.ASCII.GetBytes("http://{0}").Concat(new byte[] { 0x00 }).ToArray();
-			bool success = true;
-
-			if(!checkReadyForPatching(hProcess, b, v)) {
-				CloseHandle(hProcess);
-				return;
-			}
-
-			// WriteProcessMemory(hProcess, b + v.certpin, new byte[] {0xEB}, 1, out byteswritten); // bypass cert pinning
-			success &= VirtualProtectEx(hProcess, b + v.auth1, 0x200, PAGE_EXECUTE_READWRITE, out oldprotectflags);
-			success &= WriteProcessMemory(hProcess, b + v.auth1, new byte[] { 0xEB }, 1, out byteswritten); // send auth header for all protocols
-			success &= WriteProcessMemory(hProcess, b + v.auth2, new byte[] { 0x90, 0x90, 0x90, 0x90, 0x90, 0x90 }, 6, out byteswritten); // always send auth header
-
-			success &= WriteProcessMemory(hProcess, b + v.url, newurl, (uint)newurl.Length, out byteswritten); // (setting current)
-
-			success &= VirtualProtectEx(hProcess, b + v.protocol1, 0x20, PAGE_READWRITE, out oldprotectflags);
-			success &= WriteProcessMemory(hProcess, b + v.protocol1, http, (uint)http.Length, out byteswritten); // replace https with http
-			success &= VirtualProtectEx(hProcess, b + v.protocol2, 1, PAGE_EXECUTE_READWRITE, out oldprotectflags);
-			success &= WriteProcessMemory(hProcess, b + v.protocol2, new byte[] { (byte)http.Length }, 1, out byteswritten);
-
-			CloseHandle(hProcess);
-			if (success)
-			{
-				log(String.Format("Sucessfully patched processid {0}", process.Id));
-				log(String.Format("Injected server: {0}", getSelectedServerHostname()));
-				patchedprocesses.Add(process.Id);
-			}
-			else
-			{
-				throw new Win32Exception(Marshal.GetLastWin32Error());
-			}
-		}
-
-		private bool checkReadyForPatching(IntPtr hProcess, IntPtr baseAddress, Hitman2Version version)
-		{
-			byte[] buffer = { 0 };
-			int bytesread;
-			if (!ReadProcessMemory(hProcess, baseAddress + version.url, buffer, 1, out bytesread))
-			{
-				throw new Win32Exception(Marshal.GetLastWin32Error());
-			}
-			return buffer[0] != 0;
 		}
 
 		private void button1_Click(object sender, EventArgs e)
---
+++ Patcher/MemoryPatcher.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Hitman2Patcher
+{
+	class MemoryPatcher
+	{
+		[DllImport("kernel32", SetLastError = true)]
+		private static extern IntPtr OpenProcess(uint dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+		[DllImport("kernel32", SetLastError = true)]
+		private static extern bool CloseHandle(IntPtr hObject);
+		[DllImport("kernel32")]
+		private static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr address, byte[] buffer, uint size, out int byteswritten);
+		[DllImport("kernel32.dll", SetLastError = true)]
+		static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr address, [Out] byte[] buffer, uint size, out int numberOfBytesRead);
+		[DllImport("kernel32.dll")]
+		static extern bool VirtualProtectEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, uint flNewProtect, out uint lpflOldProtect);
+
+		private const uint PROCESS_VM_READ = 0x0010; //Required to read memory in a process using ReadProcessMemory.
+		private const uint PROCESS_VM_WRITE = 0x0020; // Required to write to memory in a process using WriteProcessMemory.
+		private const uint PROCESS_VM_OPERATION = 0x0008; // Required to perform an operation on the address space of a process using VirtualProtectEx
+		private const uint PAGE_EXECUTE_READWRITE = 0x40;
+		private const uint PAGE_READWRITE = 0x04;
+
+		public static bool Patch(Process process, Options patchOptions)
+		{
+			IntPtr hProcess = OpenProcess(PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_VM_OPERATION, false, process.Id);
+			Hitman2Version v = Hitman2Version.getVersion(process);
+			IntPtr b = process.MainModule.BaseAddress;
+			int byteswritten;
+			uint oldprotectflags;
+			byte[] newurl = Encoding.ASCII.GetBytes(patchOptions.CustomConfigDomain).Concat(new byte[] { 0x00 }).ToArray();
+			byte[] http = Encoding.ASCII.GetBytes("http://{0}").Concat(new byte[] { 0x00 }).ToArray();
+			bool success = true;
+
+			if (!IsReadyForPatching(hProcess, b, v))
+			{
+				CloseHandle(hProcess);
+				return false;
+			}
+
+			if (patchOptions.DisableCertPinning)
+			{
+				success &= VirtualProtectEx(hProcess, b + v.certpin, 1, PAGE_EXECUTE_READWRITE, out oldprotectflags);
+				success &= WriteProcessMemory(hProcess, b + v.certpin, new byte[] { 0xEB }, 1, out byteswritten); // bypass cert pinning
+			}
+			if (patchOptions.AlwaysSendAuthHeader)
+			{
+				success &= VirtualProtectEx(hProcess, b + v.auth1, 0x200, PAGE_EXECUTE_READWRITE, out oldprotectflags);
+				success &= WriteProcessMemory(hProcess, b + v.auth1, new byte[] {0xEB}, 1, out byteswritten); // send auth header for all protocols
+				success &= WriteProcessMemory(hProcess, b + v.auth2, new byte[] {0x90, 0x90, 0x90, 0x90, 0x90, 0x90}, 6,
+					out byteswritten); // always send auth header
+			}
+			if (patchOptions.SetCustomConfigDomain)
+			{
+				success &= WriteProcessMemory(hProcess, b + v.url, newurl, (uint)newurl.Length, out byteswritten); // (setting current)
+			}
+			if (patchOptions.UseHttp)
+			{
+				success &= VirtualProtectEx(hProcess, b + v.protocol1, 0x20, PAGE_READWRITE, out oldprotectflags);
+				success &= WriteProcessMemory(hProcess, b + v.protocol1, http, (uint)http.Length, out byteswritten); // replace https with http
+				success &= VirtualProtectEx(hProcess, b + v.protocol2, 1, PAGE_EXECUTE_READWRITE, out oldprotectflags);
+				success &= WriteProcessMemory(hProcess, b + v.protocol2, new byte[] {(byte)http.Length}, 1, out byteswritten);
+			}
+
+			CloseHandle(hProcess);
+			if (!success)
+			{
+				throw new Win32Exception(Marshal.GetLastWin32Error());
+			}
+			return true;
+		}
+
+		private static bool IsReadyForPatching(IntPtr hProcess, IntPtr baseAddress, Hitman2Version version)
+		{
+			byte[] buffer = { 0 };
+			int bytesread;
+			if (!ReadProcessMemory(hProcess, baseAddress + version.url, buffer, 1, out bytesread))
+			{
+				throw new Win32Exception(Marshal.GetLastWin32Error());
+			}
+			return buffer[0] != 0;
+		}
+
+		public struct Options
+		{
+			public bool DisableCertPinning;
+			public bool AlwaysSendAuthHeader;
+			public bool SetCustomConfigDomain;
+			public string CustomConfigDomain;
+			public bool UseHttp;
+		}
+	}
+}
