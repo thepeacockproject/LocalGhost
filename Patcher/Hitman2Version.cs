e101621a83429d389e8a7514b3929484f214649a
Govert de Gans <grappigegovert@hotmail.com>
2020-11-15 18:10:37 +0100

    Patcher: support v2.72.0.0 hotfix 5

--- Patcher/Hitman2Version.cs
+++ Patcher/Hitman2Version.cs
@@ -47,6 +47,10 @@ namespace Hitman2Patcher
 		{
 			switch (timestamp)
 			{
+				case 0x5F8D57CA:
+					return "2.72.0.0-h5_dx11";
+				case 0x5F8D56D3:
+					return "2.72.0.0-h5_dx12";
 				case 0x5EE9D065:
 					return "2.72.0.0-h4_dx11";
 				case 0x5EE9D095:
@@ -107,10 +111,44 @@ namespace Hitman2Patcher
 			}
 		};
 
+		public static Hitman2Version v2_72_0_h5_dx11 = new Hitman2Version()
+		{
+			certpin = new[] { new Patch(0x0F32FAE, "0F85", "90E9", MemProtection.PAGE_EXECUTE_READ) },
+			authheader = new[]
+			{
+				new Patch(0x0B5A1F8, "75", "EB", MemProtection.PAGE_EXECUTE_READ),
+				new Patch(0x0B5A21C, "0F8486000000", "909090909090", MemProtection.PAGE_EXECUTE_READ)
+			},
+			configdomain = new[] { new Patch(0x2BBBC08, "", "", MemProtection.PAGE_READWRITE, "configdomain") },
+			protocol = new[]
+			{
+				new Patch(0x182D598, https, http, MemProtection.PAGE_READONLY),
+				new Patch(0x0B4ED64, "0C", "0B", MemProtection.PAGE_EXECUTE_READ)
+			}
+		};
+
+		public static Hitman2Version v2_72_0_h5_dx12 = new Hitman2Version()
+		{
+			certpin = new[] { new Patch(0x0F32B0E, "0F85", "90E9", MemProtection.PAGE_EXECUTE_READ) },
+			authheader = new[]
+			{
+				new Patch(0x0B59D58, "75", "EB", MemProtection.PAGE_EXECUTE_READ),
+				new Patch(0x0B59D7C, "0F8486000000", "909090909090", MemProtection.PAGE_EXECUTE_READ)
+			},
+			configdomain = new[] { new Patch(0x2BDA208, "", "", MemProtection.PAGE_READWRITE, "configdomain") },
+			protocol = new[]
+			{
+				new Patch(0x18486B8, https, http, MemProtection.PAGE_READONLY),
+				new Patch(0x0B4E8C4, "0C", "0B", MemProtection.PAGE_EXECUTE_READ)
+			}
+		};
+
 		private static Dictionary<string, Hitman2Version> versionMap = new Dictionary<string, Hitman2Version>()
 		{
 			{"2.72.0.0-h4_dx11", v2_72_0_h4_dx11},
 			{"2.72.0.0-h4_dx12", v2_72_0_h4_dx12},
+			{"2.72.0.0-h5_dx11", v2_72_0_h5_dx11},
+			{"2.72.0.0-h5_dx12", v2_72_0_h5_dx12},
 		};
 	}
 }
