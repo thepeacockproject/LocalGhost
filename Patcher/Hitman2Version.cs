8b2649ef9d53340ef3c1b7ebf42bd35a48e6f23f
Govert de Gans <grappigegovert@hotmail.com>
2021-01-04 07:03:53 +0100

    Patcher: Move patch definitions to separate file

--- Patcher/Hitman2Version.cs
+++ Patcher/Hitman2Version.cs
@@ -41,26 +41,29 @@ namespace Hitman2Patcher
 	{
 		public Patch[] certpin, authheader, configdomain, protocol, dynres_noforceoffline;
 
-		private Hitman2Version()
-		{
+		private static Dictionary<uint, string> timestampMap = new Dictionary<uint, string>();
 
+		private static Dictionary<string, Hitman2Version> versionMap = new Dictionary<string, Hitman2Version>();
+
+		public static IEnumerable<string> Versions
+		{
+			get { return versionMap.Keys; }
+		}
+
+		public static void addVersion(string name, uint timestamp, Hitman2Version patchVersions)
+		{
+			timestampMap.Add(timestamp, name);
+			versionMap.Add(name, patchVersions);
 		}
 
 		private static string versionStringFromTimestamp(UInt32 timestamp)
 		{
-			switch (timestamp)
+			string result;
+			if (!timestampMap.TryGetValue(timestamp, out result))
 			{
-				case 0x5F8D57CA:
-					return "2.72.0.0-h5_dx11";
-				case 0x5F8D56D3:
-					return "2.72.0.0-h5_dx12";
-				case 0x5EE9D065:
-					return "2.72.0.0-h4_dx11";
-				case 0x5EE9D095:
-					return "2.72.0.0-h4_dx12";
-				default:
-					return "unknown";
+				result = "unknown";
 			}
+			return result;
 		}
 
 		public static Hitman2Version getVersion(UInt32 timestamp, string versionString = "")
@@ -79,69 +82,9 @@ namespace Hitman2Patcher
 			throw new NotImplementedException();
 		}
 
-		public static Hitman2Version v2_72_0_h4_dx11 = new Hitman2Version()
+		static Hitman2Version()
 		{
-			certpin = new[] { new Patch(0x0F33363, "75", "EB", MemProtection.PAGE_EXECUTE_READ) },
-			authheader = new[]
-			{
-				new Patch(0x0B5A1F8, "75", "EB", MemProtection.PAGE_EXECUTE_READ),
-				new Patch(0x0B5A21C, "0F8486000000", "909090909090", MemProtection.PAGE_EXECUTE_READ)
-			},
-			configdomain = new[] { new Patch(0x2BBBC08, "", "", MemProtection.PAGE_READWRITE, "configdomain") },
-			protocol = new[]
-			{
-				new Patch(0x182D598, Patch.https, Patch.http, MemProtection.PAGE_READONLY),
-				new Patch(0x0B4ED64, "0C", "0B", MemProtection.PAGE_EXECUTE_READ)
-			},
-			dynres_noforceoffline = new[] { new Patch(0x2BBC548, "01", "00", MemProtection.PAGE_READWRITE) }
-		};
-
-		public static Hitman2Version v2_72_0_h4_dx12 = new Hitman2Version()
-		{
-			certpin = new[] { new Patch(0x0F32EC3, "75", "EB", MemProtection.PAGE_EXECUTE_READ) },
-			authheader = new[]
-			{
-				new Patch(0x0B59D58, "75", "EB", MemProtection.PAGE_EXECUTE_READ),
-				new Patch(0x0B59D7C, "0F8486000000", "909090909090", MemProtection.PAGE_EXECUTE_READ)
-			},
-			configdomain = new[] { new Patch(0x2BDA208, "", "", MemProtection.PAGE_READWRITE, "configdomain") },
-			protocol = new[]
-			{
-				new Patch(0x18486B8, Patch.https, Patch.http, MemProtection.PAGE_READONLY),
-				new Patch(0x0B4E8C4, "0C", "0B", MemProtection.PAGE_EXECUTE_READ)
-			},
-			dynres_noforceoffline = new[] { new Patch(0x2BDAB48, "01", "00", MemProtection.PAGE_EXECUTE_READWRITE) }
-		};
-
-		public static Hitman2Version v2_72_0_h5_dx11 = new Hitman2Version()
-		{
-			certpin = new[] { new Patch(0x0F32FAE, "0F85", "90E9", MemProtection.PAGE_EXECUTE_READ) },
-			authheader = v2_72_0_h4_dx11.authheader,
-			configdomain = v2_72_0_h4_dx11.configdomain,
-			protocol = v2_72_0_h4_dx11.protocol,
-			dynres_noforceoffline = v2_72_0_h4_dx11.dynres_noforceoffline
-		};
-
-		public static Hitman2Version v2_72_0_h5_dx12 = new Hitman2Version()
-		{
-			certpin = new[] { new Patch(0x0F32B0E, "0F85", "90E9", MemProtection.PAGE_EXECUTE_READ) },
-			authheader = v2_72_0_h4_dx12.authheader,
-			configdomain = v2_72_0_h4_dx12.configdomain,
-			protocol = v2_72_0_h4_dx12.protocol,
-			dynres_noforceoffline = v2_72_0_h4_dx12.dynres_noforceoffline
-		};
-
-		private static Dictionary<string, Hitman2Version> versionMap = new Dictionary<string, Hitman2Version>()
-		{
-			{"2.72.0.0-h4_dx11", v2_72_0_h4_dx11},
-			{"2.72.0.0-h4_dx12", v2_72_0_h4_dx12},
-			{"2.72.0.0-h5_dx11", v2_72_0_h5_dx11},
-			{"2.72.0.0-h5_dx12", v2_72_0_h5_dx12},
-		};
-
-		public static IEnumerable<string> Versions
-		{
-			get { return versionMap.Keys; }
+			v2_72.addVersions();
 		}
 	}
 }
