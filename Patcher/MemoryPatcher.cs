cb88534d61775d9209377459c3fd81066590708f
Govert de Gans <grappigegovert@hotmail.com>
2021-07-02 18:41:44 +0200

    Patcher: add some [Out] attributes to the P/Invoked functions Will this fix the mysterious 299 errors?

--- Patcher/MemoryPatcher.cs
+++ Patcher/MemoryPatcher.cs
@@ -35,12 +35,12 @@ namespace HitmanPatcher
 		private static extern bool CloseHandle(IntPtr hObject);
 
 		[DllImport("kernel32.dll", SetLastError = true)]
-		private static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr address, byte[] buffer, UIntPtr size, out UIntPtr byteswritten);
+		private static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr address, byte[] buffer, UIntPtr size, [Out] out UIntPtr byteswritten);
 
 		[DllImport("kernel32.dll", SetLastError = true)]
-		private static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr address, [Out] byte[] buffer, UIntPtr size, out UIntPtr numberOfBytesRead);
+		private static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr address, [Out] byte[] buffer, UIntPtr size, [Out] out UIntPtr numberOfBytesRead);
 
-		[DllImport("kernel32.dll")]
+		[DllImport("kernel32.dll", SetLastError = true)]
 		private static extern bool VirtualProtectEx(IntPtr hProcess, IntPtr lpAddress, UIntPtr dwSize, MemProtection flNewProtect, out MemProtection lpflOldProtect);
 
 		// from https://docs.microsoft.com/en-us/windows/win32/procthread/process-security-and-access-rights
