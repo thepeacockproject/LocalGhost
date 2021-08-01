9666b485cc5a22f1e23de7c7e985fe765e3a5d6c
Govert de Gans <grappigegovert@hotmail.com>
2021-08-01 16:57:31 +0200

    Patcher: rename v2.71.0-h1 to v2.71.0 because the exe was not changed that hotfix only changed some content

--- Patcher/PatchDefinitions/v2_71.cs
+++ Patcher/PatchDefinitions/v2_71.cs
@@ -7,11 +7,11 @@ namespace HitmanPatcher
 	{
 		public static void addVersions()
 		{
-			HitmanVersion.addVersion("2.71.0.0-h1_dx11", 0x5D9DEA3E, v2_71_0_h1_dx11);
-			HitmanVersion.addVersion("2.71.0.0-h1_dx12", 0x5D9DEA53, v2_71_0_h1_dx12);
+			HitmanVersion.addVersion("2.71.0.0_dx11", 0x5D9DEA3E, v2_71_0_dx11);
+			HitmanVersion.addVersion("2.71.0.0_dx12", 0x5D9DEA53, v2_71_0_dx12);
 		}
 
-		private static HitmanVersion v2_71_0_h1_dx11 = new HitmanVersion()
+		private static HitmanVersion v2_71_0_dx11 = new HitmanVersion()
 		{
 			certpin = new[] { new Patch(0x0F33293, "75", "EB", MemProtection.PAGE_EXECUTE_READ) },
 			authheader = new[]
@@ -28,7 +28,7 @@ namespace HitmanPatcher
 			dynres_noforceoffline = new[] { new Patch(0x2BBBF28, "01", "00", MemProtection.PAGE_READWRITE) }
 		};
 
-		public static HitmanVersion v2_71_0_h1_dx12 = new HitmanVersion()
+		public static HitmanVersion v2_71_0_dx12 = new HitmanVersion()
 		{
 			certpin = new[] { new Patch(0x0F32DF3, "75", "EB", MemProtection.PAGE_EXECUTE_READ) },
 			authheader = new[]
