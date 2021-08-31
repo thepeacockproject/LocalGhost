aeacc5b51621820af0cf6502b966e2db141cb877
Govert de Gans <grappigegovert@hotmail.com>
2021-08-31 14:25:13 +0200

    Patcher: Add support for v3.50.0 hotfix 1

--- Patcher/PatchDefinitions/v3_50.cs
+++ Patcher/PatchDefinitions/v3_50.cs
@@ -8,6 +8,7 @@ namespace HitmanPatcher
 		public static void addVersions()
 		{
 			HitmanVersion.addVersion("3.50.0.0_dx12", 0x60F92990, v3_50_0_dx12);
+			HitmanVersion.addVersion("3.50.0.0-h1_dx12", 0x611E4422, v3_50_0_h1_dx12);
 		}
 
 		private static HitmanVersion v3_50_0_dx12 = new HitmanVersion()
@@ -24,6 +25,18 @@ namespace HitmanPatcher
 				new Patch(0x1E6FBA0, "68", "61", MemProtection.PAGE_READONLY) // dont ask me why this works
 			},
 			dynres_noforceoffline = new[] { new Patch(0x3B19758, "01", "00", MemProtection.PAGE_EXECUTE_READWRITE) }
+		};
+
+		private static HitmanVersion v3_50_0_h1_dx12 = new HitmanVersion()
+		{
+			certpin = v3_50_0_dx12.certpin,
+			authheader = v3_50_0_dx12.authheader,
+			configdomain = v3_50_0_dx12.configdomain,
+			protocol = new[]
+			{
+				new Patch(0x1E6FB60, "68", "61", MemProtection.PAGE_READONLY) // dont ask me why this works
+			},
+			dynres_noforceoffline = v3_50_0_dx12.dynres_noforceoffline
 		};
 	}
 }
