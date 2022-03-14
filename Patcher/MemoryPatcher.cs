thepeacockproject/LocalGhost

commit 23bb7baf3fd2cee7d9aaa388f3272557a5c81e7e

Patcher: Yet another possible fix for the elusive 299 error

Patcher/MemoryPatcher.cs
@@ -139,11 +139,22 @@ namespace HitmanPatcher
 			byte[] buffer = { 0 };
 			UIntPtr bytesread;
 			bool ready = true;
+			MemProtection newmemprotection = MemProtection.PAGE_READWRITE;
+			// It should already be READWRITE, but this is to remove possible PAGE_GUARD temporarily
 			foreach (Patch p in version.configdomain.Where(p => p.customPatch == "configdomain"))
 			{
+				MemProtection oldprotectflags;
+				if (!Pinvoke.VirtualProtectEx(hProcess, baseAddress + p.offset, (UIntPtr)p.patch.Length, newmemprotection, out oldprotectflags))
+				{
+					throw new Win32Exception(Marshal.GetLastWin32Error(), string.Format("error at vpe1Check for offset {0:X}", p.offset));
+				}
 				if (!Pinvoke.ReadProcessMemory(hProcess, baseAddress + p.offset, buffer, (UIntPtr)1, out bytesread))
 				{
-					throw new Win32Exception(Marshal.GetLastWin32Error());
+					throw new Win32Exception(Marshal.GetLastWin32Error(), string.Format("error at rpmCheck for offset {0:X}", p.offset));
+				}
+				if (!Pinvoke.VirtualProtectEx(hProcess, baseAddress + p.offset, (UIntPtr)p.patch.Length, oldprotectflags, out oldprotectflags))
+				{
+					throw new Win32Exception(Marshal.GetLastWin32Error(), string.Format("error at vpe2Check for offset {0:X}", p.offset));
 				}
 				ready &= buffer[0] != 0;
 			}

Patcher/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.34.0.330")]
-[assembly: AssemblyFileVersion("2.34.0.330")]
+[assembly: AssemblyVersion("2.35.0.331")]
+[assembly: AssemblyFileVersion("2.35.0.331")]

commit cb7f4f3b8fdfa9aaf5f2fce17c34eaf1db2deba3

Fix error code 87 bug in patch ready check introduced in 23bb7baf (#4)

Patcher/MemoryPatcher.cs
@@ -144,7 +144,7 @@ namespace HitmanPatcher
 			foreach (Patch p in version.configdomain.Where(p => p.customPatch == "configdomain"))
 			{
 				MemProtection oldprotectflags;
-				if (!Pinvoke.VirtualProtectEx(hProcess, baseAddress + p.offset, (UIntPtr)p.patch.Length, newmemprotection, out oldprotectflags))
+				if (!Pinvoke.VirtualProtectEx(hProcess, baseAddress + p.offset, (UIntPtr)1, newmemprotection, out oldprotectflags))
 				{
 					throw new Win32Exception(Marshal.GetLastWin32Error(), string.Format("error at vpe1Check for offset {0:X}", p.offset));
 				}
@@ -152,7 +152,7 @@ namespace HitmanPatcher
 				{
 					throw new Win32Exception(Marshal.GetLastWin32Error(), string.Format("error at rpmCheck for offset {0:X}", p.offset));
 				}
-				if (!Pinvoke.VirtualProtectEx(hProcess, baseAddress + p.offset, (UIntPtr)p.patch.Length, oldprotectflags, out oldprotectflags))
+				if (!Pinvoke.VirtualProtectEx(hProcess, baseAddress + p.offset, (UIntPtr)1, oldprotectflags, out oldprotectflags))
 				{
 					throw new Win32Exception(Marshal.GetLastWin32Error(), string.Format("error at vpe2Check for offset {0:X}", p.offset));
 				}

Patcher/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.35.0.331")]
-[assembly: AssemblyFileVersion("2.35.0.331")]
+[assembly: AssemblyVersion("2.35.1.333")]
+[assembly: AssemblyFileVersion("2.35.1.333")]