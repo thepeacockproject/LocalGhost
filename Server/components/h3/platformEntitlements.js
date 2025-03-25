4a01a59477d13edb4902e5c3ea65c4ca05a89996
Govert de Gans <grappigegovert@hotmail.com>
2025-03-25 06:00:35 +0100

    Update h3 epic entitlements

--- Server/components/h3/platformEntitlements.js
+++ Server/components/h3/platformEntitlements.js
@@ -12,24 +12,28 @@ function GetPlatformEntitlements(req, res) {
             '66246e4364134f4689da72e9c6731687', // Hitman 3: Chongquing
             '4216cdf59dbc4f19af227be076520202', // Hitman 3: Mendoza
             '8a690003855745e884d5040c6bc9ede8', // Hitman 3: Carpathian Mountains
-            'bc610b36c75442299edcbe99f6f0fb60', // Hitman 3 - Deluxe Pack
             '5d06a6c6af9b4875b3530d5328f61287', // Hitman 3 - Trinity Pack
             '0b59243cb8aa420691b66be1ecbe68c0', // HITMAN 1 Standard
             '894d1e6771044f48a8fdde934b8e443a', // HITMAN 1 GOTY Upgrade
-            'e698e1a4b63947b0bc9349a5ae2dc015', // ? (HITMAN 1 Requiem pack?) (does nothing?) (HITMAN 1 GOTY according to scream-db.web.app)
             '391d08a543dc43a083eb50246916a291', // Hitman 3 Access Pass: HITMAN 2 Standard
             'afa4b921503f43339c360d4b53910791', // Hitman 3 Access Pass: HITMAN 2 Expansion
-            '6408de14f7dc46b9a33adcf6cbc4d159', // ? (HITMAN 2 Executive pack?) (does nothing?) (HITMAN 2 Gold according to scream-db.web.app)
-            'b4e2e682cecd42b3a7017ee4838b4593', // ? (EiderGoldEditionAudience according to epicdata.info) (included in H3 Deluxe Pre-purchase)
-            '1dea1e39a8044a69b4020845afb4bd97', // ? (included in H3 Pre-purchase)
-            'feeac4b521734f22ae99e8ac55a5f896', // ? (included in H3 Pre-purchase)
+            'bc610b36c75442299edcbe99f6f0fb60', // Hitman 3 - Deluxe Pack
+            'c141ab4a97c44cffb876695ea42fc7c5', // Episode: Sapienza
             '0e8632b4cdfb415e94291d97d727b98d', // Deadly Sins 1: Greed
-            '3f9adc216dde44dda5e829f11740a0a2', // (Deadly Sins 2: Pride?)
-            'aece009ff59441c0b526f8aa69e24cfb', // (Deadly Sins 3: Sloth?)
-            'dfe5aeb89976450ba1e0e2c208b63d33', // (Deadly Sins 4: Lust?)
-            '30107bff80024d1ab291f9cd3bac9fac', // (Deadly Sins 5: Gluttony?)
-            '0403062df0d347619c8dcf043c65c02e', // (Deadly Sins 6: Wrath?)
-            '9e936ed2507a473db6f53ad24d2da587', // (Deadly Sins 7: Envy?)
+            '3f9adc216dde44dda5e829f11740a0a2', // Deadly Sins 2: Pride
+            'aece009ff59441c0b526f8aa69e24cfb', // Deadly Sins 3: Sloth
+            'dfe5aeb89976450ba1e0e2c208b63d33', // Deadly Sins 4: Lust
+            '30107bff80024d1ab291f9cd3bac9fac', // Deadly Sins 5: Gluttony
+            '9e936ed2507a473db6f53ad24d2da587', // Deadly Sins 7: Wrath
+            '0403062df0d347619c8dcf043c65c02e', // Deadly Sins 6: Envy
+            '28455871cd0d4ffab52f557cc012ea5e', // Sarajevo Six
+            'a1e9a63fa4f3425aa66b9b8fa3c9cc35', // Street Art Pack
+            '08d2bc4d20754191b6c488541d2b4fa1', // Makeshift Pack
+            '9220c020262f420da06eb46a4b1ce86f', // The Undying Pack
+            '6cdf07da030d4f66acd50eaf3cd234c7', // The Disruptor Pack
+            'f04198e0ffcf49079b5ec77bb6b66891', // The Drop Pack
+            '70a9afcc8de84b6ab0f2b45b2018559b', // The Splitter pack
+            '84a1a6fda4fb48afbb78ee9b2addd475', // WoA Deluxe Pack
         ]);
     } else if (
         req.body.issuerId === '1659040' // Hitman 3 steam retail appid
